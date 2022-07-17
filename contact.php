52694968c9aa4b0b15723e529a9e81f9b15d91b7
Ronnie Ineza <ronnie@Ronnies-MacBook-Pro.local>
2022-07-17 21:48:19 +0300

    created contact page and php scripts

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="ip2_contact";
+
+try{
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br />";
+}
+catch(PDOException $e){
+
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$query= $conn->prepare("INSERT INTO Messages(name,email,phone,message) VALUES
+(?,?,?,?)");
+$query->bindParam(1, $name);
+$query->bindParam(2, $email);
+$query->bindParam(3, $phone);
+$query->bindParam(4, $message);
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$message=$_POST['message'];
+$query->execute();
+$conn = null;
+echo 'Hi '.$_POST['name'].' ' .'thanks for your interest.</br>';
+echo 'We will contact you at '. $_POST['email'] .' very soon.<br />';
+?>
---
+++ create.php
@@ -0,0 +1,26 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="ip2_contact";
+
+try{
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+}
+catch(PDOException $e){
+
+    echo "Connection failed: " . $e->getMessage();
+
+}
+$query = $conn ->prepare("CREATE TABLE Messages (id int(6) NOT NULL
+auto_increment,name varchar(30) NOT NULL, NOT NULL,email varchar(30) NOT NULL, phone varchar(30) NOT NULL, messages varchar(30) NOT NULL,PRIMARY
+KEY (id),UNIQUE id (id),KEY id_2 (id))");
+
+$query -> execute();
+$conn = null;
+
+?>
