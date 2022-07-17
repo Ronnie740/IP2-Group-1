d461ed44e5ea5613e53bb0dceeed719b0c2ade7c
Ronnie Ineza <ronnie@Ronnies-MacBook-Pro.local>
2022-07-17 23:00:36 +0300

    added coment

--- createDB.php
+++ createDB.php
@@ -5,7 +5,7 @@ $password = "";
 
 try {
   $conn = new PDO("mysql:host=$servername", $username, $password);
-  // set the PDO error mode to exception
+  // set the PDO error mode to exception hello
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully <br />";
   }
